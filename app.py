7dcba09f05e8746500116d7d00c9f5804afe931f
Shadow Beast <66586113+Chenura@users.noreply.github.com>
2026-05-04 12:49:32 +0530

    Update model version to 'gemini-1.5-flash-latest'

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 bot = telegram.Bot(token=BOT_TOKEN)
 app = Flask(__name__)
